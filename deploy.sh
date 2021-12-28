027d454674e713de4d814d11d5f9384369a6f3b0
Cheng Liang <ceelia0@163.com>
2021-12-28 15:54:49 +0800

    Update

--- deploy.sh
+++ deploy.sh
@@ -2,6 +2,8 @@
 set -e
 git add -A
 git commit -m 'update'
+#git commit -a -m 'update'
+
 eval `ssh-agent`
 ssh-add /home/ceelia/.ssh/github_id_rsa
 git push -f git@github.com:celiae/celiae.github.io.git main
