4036fba236bfc90f5a6abfb42ab2ab17ce91e26b
Cheng Liang <ceelia0g@gmail.com>
2022-03-06 19:44:16 +0800

    source code

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
-## Celiae Space
+# Celiae Space
 
+### 关于我的个人主页，我与这个世界的连接
 
+[进入Celiae Space](https://celiae.github.io)
---
+++ deploy.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/env sh
+
+# abort on errors
+set -e
+
+# ssh
+eval `ssh-agent`
+ssh-add /home/ceelia/.ssh/github_id_rsa
+
+# push source code
+git add -A
+git commit -m 'source code'
+git push origin main
+# git push -f git@github.com:celiae/ceblog.git main
+
+# build
+yarn build
+
+# navigate into the build output directory
+cd dist
+
+# if you are deploying to a custom domain
+# echo 'ceelia.v6.navy' > CNAME
+
+git init
+git branch -m gh-pages
+git add -A
+git commit -m 'deploy'
+
+# if you are deploying to https://<USERNAME>.github.io
+# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
+
+# if you are deploying to https://<USERNAME>.github.io/<REPO>
+git push -f git@github.com:celiae/celiae.github.io.git gh-pages
+
+cd -
